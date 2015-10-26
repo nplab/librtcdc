49226bad3b59c7cad9be66d8476a72e0efef3a19
Felix Weinrank <weinrank@fh-muenster.de>
2015-10-26 14:54:39 +0100

    Sync

--- python/example.py
+++ python/example.py
@@ -3,22 +3,32 @@
 import pyrtcdc
 import base64
 
+# called when connected to remote peer
+def on_connect(peer):
+  print 'on_connect!\n'
+  peer.create_data_channel('demo', on_open=on_open)
+
+# called when channel is opened
+def on_open(channel):
+  print 'OPEN!!!!\n'
+  channel.on_message = on_message
+  channel.send(pyrtcdc.DATATYPE_STRING, 'Hi')
+
 def on_channel(peer, channel):
-  print 'new channel %s created' %(channel.label)
+  print 'new channel %s created\n' %(channel.label)
   channel.on_message = on_message
 
 def on_candidate(peer, candidate):
   print 'local candidate sdp:\n%s' %(candidate)
 
 def on_message(channel, datatype, data):
-  print 'received data from channel %s: %s' %(channel.label, data)
+  print 'received data from channel %s: %s\n' %(channel.label, data)
   channel.send(pyrtcdc.DATATYPE_STRING, 'hi')
 
-peer = pyrtcdc.PeerConnection(on_channel, on_candidate, stun_server='stun.services.mozilla.com')
+peer = pyrtcdc.PeerConnection(on_channel, on_candidate, on_connect, stun_server='stun.services.mozilla.com')
 
 offer = peer.generate_offer()
 print 'base64 encoded local offer sdp:\n%s\n' %(base64.b64encode(offer))
-
 print 'enter base64 encoded remote offer sdp:'
 while True:
   roffer64 = raw_input('> ')
@@ -41,4 +51,5 @@ while True:
   print 'invalid remote candidate sdp'
   print 'enter remote candidate sdp:'
 
+print 'waiting ...\n'
 peer.loop()
